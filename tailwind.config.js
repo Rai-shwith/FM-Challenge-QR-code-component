8678a90ebcac70695de8498226808f6d969c7cf8
Rai-shwith <ashwith96.96@gmail.com>
2024-07-23 16:22:34 +0530

    Task finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./script.js"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '340': '340px', // add this line
+      },
+    },
   },
   plugins: [],
 }
